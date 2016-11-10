11133e3a8cb46d6dad28b4ff650ea473e55ad5d9
Veerakit Thanyawaechakit <Veerakit.t@tilleke.com>
2016-11-10 14:30:25 +0700

    Add CarRepo adn CarService

---
+++ CarFuel.Data/CarRepository.cs
@@ -0,0 +1,14 @@
+﻿using CarFuel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace CarFuel.Data {
+    public class CarRepository : RepositoryBase<Car> {
+        public CarRepository(DbContext context) : base(context) {
+        }
+    }
+}
